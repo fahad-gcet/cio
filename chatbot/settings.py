a7b8a5ca8cfbac4e7c2cfc30c617291799b04d97
fahad-gcet <fms525@outlook.com>
2017-02-20 04:12:46 +0530

    add allowed host

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'fsch+6!=q+@ol&%0x!nwdl@48^ixbd4clx5f1i!5n^66y+pmn*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['139.59.14.102']
 
 
 # Application definition
